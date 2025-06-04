6613ed0d5390842f6ef830fc9dbc1a589a552d68
OsTn0928 <vicenterojas@liceovvh.cl>
2025-06-04 18:29:25 -0400

    final

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-// Inicializa EmailJS (poner tu public key)
-emailjs.init("service_oo2kkvn");
+// Inicializa EmailJS
+(function() {
+  emailjs.init("gBMVDofguoJa45V5V"); // Tu Public Key
+})();
 
-const form = document.getElementById("formulario-contacto");
-const estado = document.getElementById("estado-envio");
-
-form.addEventListener("submit", function (e) {
+// Manejo del envío del formulario
+document.getElementById("contact-form").addEventListener("submit", function(e) {
   e.preventDefault();
 
-  emailjs.sendForm("service_oo2kkvn", "TU_TEMPLATE_ID", this)
-    .then(function () {
-      estado.textContent = "Mensaje enviado con éxito";
-      estado.style.color = "limegreen";
-      form.reset();
-    }, function (error) {
-      estado.textContent = "Hubo un error. Intenta nuevamente.";
-      estado.style.color = "tomato";
-      console.error("Error:", error);
+  emailjs.sendForm("service_oo2kkvn", "template_8h7nh2l", this)
+    .then(function() {
+      alert("Mensaje enviado con éxito 🚀");
+      document.getElementById("contact-form").reset(); // Limpia el formulario
+    }, function(error) {
+      console.error("Error al enviar el mensaje:", error);
+      alert("Ocurrió un error al enviar el mensaje. Inténtalo nuevamente.");
     });
 });
